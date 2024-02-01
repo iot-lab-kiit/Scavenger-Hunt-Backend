e44f908dd3f42ae218ad5a78bce380d5b1566aea
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-02-01 23:44:04 +0530

    Update package.json and model files

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import teamRoute from "./src/routes/team.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017/scaveger";
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/team", teamRoute);
 
 app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(mongo_uri)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
