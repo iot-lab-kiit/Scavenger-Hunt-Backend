94792c04bfcf683a9a841abb77f340913135d2b4
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-02-10 02:12:23 +0530

    Added firebase token verification

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { createResponse } from "../../respo.js";
+import { firebaseAuth } from "../lib/firebase-admin.js";
 dotenv.config();
 
 export const authToken = (req, res, next) => {
@@ -9,5 +10,32 @@ export const authToken = (req, res, next) => {
     if (req.headers.authorization.split(" ")[1] === process.env.ACCESS_TOKEN)
       next();
     else return res.send(createResponse(20));
+  }
+};
+
+// export const authToken = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+//     if (process.env.ACCESS_TOKEN_DISABLED === "true") next();
+//     else {
+//       if (!req.headers.authorization || !token)
+//         return res.send(createResponse(19));
+//       if (await verifyIdToken(token)) next();
+//       else return res.send(createResponse(20));
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return res.send(createResponse(16));
+//   }
+// };
+
+const verifyIdToken = async (token) => {
+  try {
+    const user = await firebaseAuth.verifyIdToken(token);
+    console.log(user);
+    return user ? true : false;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
 };
